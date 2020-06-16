e6e46b82313c1545bdbe01c75f80ae6cc8f0f7ee
Kyle <kyledunshee@Kyles-MacBook-Pro.local>
2020-06-16 10:28:21 -0600

    pushed to github

---
+++ test/controllers/api/products_controller_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class Api::ProductsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_products_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get api_products_show_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get api_products_create_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get api_products_update_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_products_destroy_url
+    assert_response :success
+  end
+
+end
